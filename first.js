dfc3a21f2c757e18890db7bfb2280eabbc71bea4
rafia bae <rafiabae36@gmail.com>
2025-10-16 12:17:09 -0700

    jawascript basic
    code done

---
+++ first.js
@@ -0,0 +1,34 @@
+console.log("nexskill")
+console.log("rafia bae")
+console.log("i love jawascript")
+fullname= "rafia bae"
+console.log(fullname)
+age = "24"
+console.log(age)
+isfollow="true"
+console.log(isfollow)
+FULLNAME="rafia" 
+fullname="mushtaqahmed"
+console.log(FULLNAME)
+console.log(fullname)
+FullName="rafia"
+_FullName="rafia"
+$FullName="rafia"
+FullName="rafia"
+
+const student={
+    fullName:"rafia",
+    age:20,
+    cgpa:3.2,
+    isPass:"true"
+};
+student["age"]=student["age"]+1
+
+
+
+
+
+console.log(student["age"])
+console.log(student.age)
+console.log(student["cgpa"])
+console.log(student["isPaa"])
